cdb7adf13cdb27c6b6780dc96e282d2decf50af1
Ana <anastasiiashershneva@gmail.com>
2022-08-31 17:48:41 +0300

    checking

--- test_search_pos.py
+++ test_search_pos.py
@@ -1,10 +1,5 @@
-import pytest
 from selene.support.shared import browser
 from selene import be, have
-
-@pytest.fixture()
-def open_browser():
-    browser.open('https://google.com')
 
 
 def test_search(open_browser):
